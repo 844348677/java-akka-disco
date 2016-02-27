a99cbce0b9fc043b0ea8180f213d986c36e7f586
844348677 <844348677@qq.com>
2016-02-27 21:59:27 +0800

    java,akka,代码结构第一次提交

---
+++ src/main/java/com/liuh/akka/java/actor/ReduceActor.java
@@ -0,0 +1,7 @@
+package com.liuh.akka.java.actor;
+
+/**
+ * Created by liuh on 2016/2/27.
+ */
+public class ReduceActor {
+}
---
+++ src/main/java/com/liuh/akka/java/messages/MapData.java
@@ -0,0 +1,7 @@
+package com.liuh.akka.java.messages;
+
+/**
+ * Created by liuh on 2016/2/27.
+ */
+public class MapData {
+}
